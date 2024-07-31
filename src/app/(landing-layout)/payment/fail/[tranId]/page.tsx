df6ea3bd5335acd08d22e18c6a90925dce7071ad
Kongkon Jowarder <kongkon4545@gmail.com>
2024-07-31 17:27:44 +0600

    sweet allert message set

--- src/app/(landing-layout)/payment/fail/[tranId]/page.tsx
+++ src/app/(landing-layout)/payment/fail/[tranId]/page.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Swal from 'sweetalert2';
 
 const PaymentFailPage = ({ params }: any) => {
     console.log(params.tranId);
+
+    useEffect(()=>{
+        if(params){
+            Swal.fire({
+                icon: "error",
+                title: "Payment Fail ! Tay Again?",
+                text: "Something went wrong!",
+              });
+        }
+    }, [params])
     return (
-        <div className='h-screen flex justify-center items-center text-6xl font-bold'>
-            <h2>Payment Fail ! Tay Again?</h2>
+        // <div className='h-screen flex justify-center items-center text-6xl font-bold'>
+        //     <h2>Payment Fail ! Tay Again?</h2>
+        // </div>
+        <div>
+
         </div>
     );
 };
--- src/app/(landing-layout)/payment/success/[tranId]/page.tsx
+++ src/app/(landing-layout)/payment/success/[tranId]/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+"use client"
+import React, { useEffect } from 'react';
+import Swal from 'sweetalert2';
 
 const PaymentSuccessSinglePage = ({ params }: any) => {
     console.log(params);
+
+    useEffect(() => {
+        if (params) {
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Payment successfully.",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+    }, [params]);
     return (
-        <div className='h-screen flex justify-center items-center text-6xl font-bold'>
-            Payment successfully
+        // <div className='h-screen flex justify-center items-center text-6xl font-bold'>
+        //     Payment successfully
             
+        // </div>
+        <div>
+
         </div>
     );
 };
